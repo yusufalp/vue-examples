ca47d84829c4eed7637c0dfd080e164db74b96c3
Yusuf Alp <alpyusuf007@gmail.com>
2022-05-31 13:02:20 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <SearchBar @searchTermChange="onSearchTermChange"></SearchBar>
+  </div>
+</template>
+
+<script>
+import SearchBar from './components/SearchBar';
+export default {
+  name: 'App',
+  components: {
+    SearchBar
+  },
+  methods: {
+    onSearchTermChange: function(searchTerm) {
+      console.log(searchTerm);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  text-align: center;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <input @input="onInput"/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SearchBar",
+  methods: {
+    onInput: function(event) {
+      this.$emit("searchTermChange", event.target.value);
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
